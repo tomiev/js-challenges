a538691f7d7bdcb47b9a525d5603bce472d79869
Todd Evans <tmeva2@student.monash.edu>
2022-05-31 13:28:06 +1000

    Refactored function to one line.

--- js-basics/dragon_slayer.js
+++ js-basics/dragon_slayer.js
@@ -1,9 +1,7 @@
 // TASK: Calculate whether a dragon slayer has enough bullets to kill all the dragons.
 // Two bullets are required to kill a dragon.
 
-function enoughBullets(bullets, dragons) {
-  return (bullets / 2) >= dragons;
-}
+const enoughBullets = (bullets, dragons) => dragons * 2 <= bullets;
 
 // Tests
 console.log(enoughBullets(20, 10)); // expect true
