8e45087580f967db7776bc20d6aa5e7d13ac9bb2
Todd Evans <tmeva2@student.monash.edu>
2022-05-31 13:59:59 +1000

    Made method for converting strings to integers.

---
+++ js-basics/string_to_number.js
@@ -0,0 +1,7 @@
+// TASK: Write a simple function converting strings to numbers
+
+const stringToNumber = string => Number(string);
+
+// Test
+console.log(stringToNumber("1404"));
+console.log(stringToNumber("-77"));
