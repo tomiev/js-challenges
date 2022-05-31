2bf1f763fb5dbf7ba17a1d270780fd288e4563fb
Todd Evans <tmeva2@student.monash.edu>
2022-05-31 14:18:04 +1000

    Clarified that stringToNumber works on non-integer numbers.

--- js-basics/string_to_number.js
+++ js-basics/string_to_number.js
@@ -1,7 +1,7 @@
-// TASK: Write a simple function converting strings to numbers
+// TASK: Write a simple function converting a string to a number
 
 const stringToNumber = string => Number(string);
 
 // Test
 console.log(stringToNumber("1404"));
-console.log(stringToNumber("-77"));
+console.log(stringToNumber("-77.88"));
