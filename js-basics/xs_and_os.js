bf80ebcbe259c3afa0c5ff17e3327c366a5b27b3
Todd Evans <tmeva2@student.monash.edu>
2022-06-01 10:28:20 +1000

    Made a function which checks if a string has an equal number of xs and os.

---
+++ js-basics/xs_and_os.js
@@ -0,0 +1,15 @@
+/* Task: Check to see if a string has the same amount of 'x's and 'o's. The method
+must return a boolean and be case insensitive. The string can contain any char. */
+
+function xo(str) {
+  const arrLower = str.toLowerCase().split('');
+  const xArr = arrLower.filter(element => element === 'x');
+  const oArr = arrLower.filter(element => element === 'o');
+  return xArr.length === oArr.length;
+}
+
+// Test
+console.log(xo('XXXOOO')); // expect true
+console.log(xo('XOXOO')); // expect false
+console.log(xo('XOXOXO')); // expect true
+console.log(xo('ABCDEFG')); // expect true
