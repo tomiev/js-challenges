6e9196d401df93f4cf542435d6ec40110644d4c2
Todd Evans <evansmtodd@gmail.com>
2022-06-01 12:12:20 +1000

    Refactored to one line. Simplified the numbers to be smaller.

--- js-basics/clock.js
+++ js-basics/clock.js
@@ -1,9 +1,7 @@
 /* Write a function which returns the time since midnight in seconds,
 given the current hour, minute, and second. */
 
-function msSinceMidnight(h, m, s) {
-  return h * 3600000 + m * 60000 + s * 1000;
-}
+const msSinceMidnight = (h, m, s) => ((h * 3600) + (m * 60) + s) * 1000;
 
 // Test
 console.log(msSinceMidnight(1, 1, 2)); // expect 3662000
