9f8f079b6c8909ba7cebe9718602eb42a49d974d
Todd Evans <evansmtodd@gmail.com>
2022-06-01 11:34:45 +1000

    Wrote function which transforms a string of numbers into a fake binary string.

--- js-basics/fake_binary.js
+++ js-basics/fake_binary.js
@@ -1,5 +1,5 @@
 // Given a string of digits, replace any digit below 5 with '0' and any digit 5
-// and above with '1'. Return the resulting string.
+// and above with '1'.
 
 
 function fakeBin(string) {
