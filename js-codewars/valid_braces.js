64fdb8681fe79002f1d94b595f48f705ccd328c6
Todd Evans <evansmtodd@gmail.com>
2022-06-02 12:47:46 +1000

    Added ESLint & updated validBraces() so that it can validate non-symmetrical brace strings.

--- js-codewars/valid_braces.js
+++ js-codewars/valid_braces.js
@@ -1,20 +1,24 @@
-/*  */
+/* Write a function that takes a string of braces, and determines if the order
+of the braces is valid. It should return true if the string is valid, and false
+if it's invalid. A string of braces is considered valid if all braces are matched
+with the correct brace. */
 
-function validBraces(braces){
-  const pairs = ['()', '[]', '{}'];
-  let valid;
-
-  for (let i = 0; i < (braces.length / 2); i++) {
-    if (pairs.includes(`${braces[i]}${braces[(braces.length - 1 - i)]}`)) {
-      valid = true;
-    } else {
-      valid = false;
+function validBraces(braces) {
+  const depth = [];
+  for (let i = 0; i < braces.length; i += 1) {
+    switch (braces[i]) {
+      case '(': case '[': case '{': depth.push(braces[i]); break;
+      case ')': if (depth.pop() !== '(') return false; break;
+      case ']': if (depth.pop() !== '[') return false; break;
+      case '}': if (depth.pop() !== '{') return false; break;
+      default:
+      // do nothing
     }
   }
-  return valid;
+  return depth.length === 0;
 }
 
 // Test
 console.log(validBraces('([])')); // expect true
 console.log(validBraces('([}]')); // expect false
-console.log(validBraces('({})[({})]')) // expect true => FAILING
+console.log(validBraces('({})[({})]')); // expect true
