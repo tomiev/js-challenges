8b19739e52188d2454a71c003bfaa47034cf12d2
Todd Evans <evansmtodd@gmail.com>
2022-06-09 00:46:06 +1000

    Removed es-lint comment

--- js-codewars/ROT13_encryptor.js
+++ js-codewars/ROT13_encryptor.js
@@ -1,4 +1,3 @@
-/* eslint-disable quote-props */
 /* ROT13 is a simple ter substitution cipher that replaces a ter with the
 ter 13 ters after it in the alphabet. ROT13 is an example of the Caesar cipher.
 Create a function that takes a string and returns the string ciphered with ROT13.
@@ -12,5 +11,5 @@ function encrypt(message) {
 }
 
 /* Tests */
-console.log(encrypt('Hello_world')); /* Expect 'Uryyb_jbeyq' */
+console.log(encrypt('Hello_world123')); /* Expect 'Uryyb_jbeyq123' */
 console.log(encrypt('This is encrypted')); /* Expect 'Guvf vf rapelcgrq' */
