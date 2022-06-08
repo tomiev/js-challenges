f851d3444765912abe391900bd6aacf55af6e7fe
Todd Evans <evansmtodd@gmail.com>
2022-06-09 00:02:35 +1000

    Shortened method for legibility

--- js-codewars/ROT13_encryptor.js
+++ js-codewars/ROT13_encryptor.js
@@ -7,63 +7,8 @@ returned as they are. */
 
 function encrypt(message) {
   let encrypted = '';
-  const alpha1 = [
-    'A',
-    'a',
-    'B',
-    'b',
-    'C',
-    'c',
-    'D',
-    'd',
-    'E',
-    'e',
-    'F',
-    'f',
-    'G',
-    'g',
-    'H',
-    'h',
-    'I',
-    'i',
-    'J',
-    'j',
-    'K',
-    'k',
-    'L',
-    'l',
-    'M',
-    'm',
-  ];
-
-  const alpha2 = [
-    'N',
-    'n',
-    'O',
-    'o',
-    'P',
-    'p',
-    'Q',
-    'q',
-    'R',
-    'r',
-    'S',
-    's',
-    'T',
-    't',
-    'U',
-    'u',
-    'V',
-    'v',
-    'W',
-    'w',
-    'X',
-    'x',
-    'Y',
-    'y',
-    'Z',
-    'z',
-  ];
+  const alpha1 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const alpha2 = 'nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM';
 
   for (let i = 0; i < message.length; i += 1) {
     const letter = message[i];
